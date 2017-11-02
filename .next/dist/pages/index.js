169c3e85ff1e7fddfba7e6036fa4edf7c2cf7121
zatedzernat <bccbew164@gmail.com>
2017-11-02 13:56:17 +0700

    factprotein

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -28,24 +28,28 @@ var _factpage = require('../components/factpage');
 
 var _factpage2 = _interopRequireDefault(_factpage);
 
+var _factprotein = require('./fact/factprotein');
+
+var _factprotein2 = _interopRequireDefault(_factprotein);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = 'D:\\healtylife\\pages\\index.js?entry';
+var _jsxFileName = 'D:\\Git Command\\react\\project\\healtylife\\pages\\index.js?entry';
 
 
 var index = function index() {
     return _react2.default.createElement('div', {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 9
+            lineNumber: 10
         }
-    }, _react2.default.createElement(_healthykitchenpage2.default, {
+    }, _react2.default.createElement(_factprotein2.default, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 10
+            lineNumber: 11
         }
     }));
 };
 
 exports.default = index;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkxpbmsiLCJGb290ZXIiLCJOZXd1cGRhdGUiLCJQYWdla2l0Y2hlbiIsIkZhY3RwYWdlIiwiaW5kZXgiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFPOzs7O0FBQ1AsQUFBTyxBQUFZOzs7O0FBQ25CLEFBQU8sQUFBZTs7OztBQUN0QixBQUFPLEFBQWlCOzs7O0FBQ3hCLEFBQU8sQUFBYzs7Ozs7Ozs7O0FBRXJCLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTsyQkFDVixjQUFBOztzQkFBQTt3QkFBQSxBQUNBO0FBREE7QUFBQSxLQUFBLGtCQUNBLEFBQUM7O3NCQUFEO3dCQUZVLEFBQ1YsQUFDQTtBQUFBO0FBQUE7QUFGSixBQU9BOztrQkFBQSxBQUFlIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkQ6L2hlYWx0eWxpZmUifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkxpbmsiLCJGb290ZXIiLCJOZXd1cGRhdGUiLCJQYWdla2l0Y2hlbiIsIkZhY3RwYWdlIiwiUHJvdGVpbiIsImluZGV4Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7O0FBQ1AsQUFBTzs7OztBQUNQLEFBQU8sQUFBWTs7OztBQUNuQixBQUFPLEFBQWU7Ozs7QUFDdEIsQUFBTyxBQUFpQjs7OztBQUN4QixBQUFPLEFBQWM7Ozs7QUFDckIsQUFBTyxBQUFhOzs7Ozs7Ozs7QUFFcEIsSUFBTSxRQUFRLFNBQVIsQUFBUSxRQUFBOzJCQUNWLGNBQUE7O3NCQUFBO3dCQUFBLEFBQ0E7QUFEQTtBQUFBLEtBQUEsa0JBQ0EsQUFBQzs7c0JBQUQ7d0JBRlUsQUFDVixBQUNBO0FBQUE7QUFBQTtBQUZKLEFBT0E7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiRDovR2l0IENvbW1hbmQvcmVhY3QvcHJvamVjdC9oZWFsdHlsaWZlIn0=
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import Footer from '../components/footer'
 import Newupdate from '../components/newupdate'
 import Pagekitchen from '../components/healthykitchenpage'
 import Factpage from '../components/factpage'
+import Protein from './fact/factprotein'
 
 const index = () => (
     <div>
-    <Pagekitchen/>
+    <Protein/>
     </div>
     
 )
